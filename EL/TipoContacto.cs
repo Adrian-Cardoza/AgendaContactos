096c9ca63f2fea5e42910d08bdbdb1d513012c9e
Adrian-Cardoza <adriancardoza2006@gmail.com>
2026-04-03 23:03:55 -0600

    TipoContacto EL

--- EL/TipoContacto.cs
+++ EL/TipoContacto.cs
@@ -8,5 +8,9 @@ namespace EL
 {
 	public class TipoContacto
 	{
+		public int TipoContactoId { get; set; }
+		public string Nombre { get; set; }
+		public string Descripcion { get; set; }
+		public byte Activo { get; set; }
 	}
 }
