1bfbd4bb4343a57237c7fba80774798ab59fb15a
JasonRivas <jasonalexander_10@hotmail.com>
2026-04-03 23:22:21 -0600

    CONTACTO EL

--- EL/Contacto.cs
+++ EL/Contacto.cs
@@ -8,5 +8,20 @@ namespace EL
 {
     public class Contacto
     {
+        public int ContactoID { get; set; }
+        public int TipoContactoId { get; set; }
+        public TipoContacto TipoContacto { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Telefono { get; set; }
+        public string Celular { get; set; }
+        public string correo { get; set; }
+        public string Direccion { get; set; }
+        public string Empresa { get; set; }
+        public string Cargo { get; set; }
+        public string Notas { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        public byte Activo { get; set; }
     }
 }
