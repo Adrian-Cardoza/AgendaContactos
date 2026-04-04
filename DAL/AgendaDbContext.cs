44038a23a9f743b2c33b1528fe2eae4f5659c3e1
Rey4l4st0r <reinaldocalleshdez@gmail.com>
2026-04-04 00:17:13 -0600

    conexion y migraciones a Db exitosas

---
+++ DAL/AgendaDbContext.cs
@@ -0,0 +1,33 @@
+﻿using EL;
+using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace DAL
+{
+    public class AgendaDbContext : DbContext
+    {
+        // El contexto se ha configurado para usar una cadena de conexión 'AgendaDbContext' del archivo 
+        // de configuración de la aplicación (App.config o Web.config). De forma predeterminada, 
+        // esta cadena de conexión tiene como destino la base de datos 'DAL.AgendaDbContext' de la instancia LocalDb. 
+        // 
+        // Si desea tener como destino una base de datos y/o un proveedor de base de datos diferente, 
+        // modifique la cadena de conexión 'AgendaDbContext'  en el archivo de configuración de la aplicación.
+        public AgendaDbContext()
+            : base("name=AgendaDbContext")
+        {
+        }
+
+        // Agregue un DbSet para cada tipo de entidad que desee incluir en el modelo. Para obtener más información 
+        // sobre cómo configurar y usar un modelo Code First, vea http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        public virtual DbSet<Contacto> Contacto { get; set; }
+        public virtual DbSet<TipoContacto> TipoContacto { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
---
+++ DAL/Migrations/202604040604259_IniciarBaseDatos.cs
@@ -0,0 +1,54 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class IniciarBaseDatos : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Contactos",
+                c => new
+                    {
+                        ContactoID = c.Int(nullable: false, identity: true),
+                        TipoContactoId = c.Int(nullable: false),
+                        Nombre = c.String(),
+                        Apellido = c.String(),
+                        Telefono = c.String(),
+                        Celular = c.String(),
+                        correo = c.String(),
+                        Direccion = c.String(),
+                        Empresa = c.String(),
+                        Cargo = c.String(),
+                        Notas = c.String(),
+                        FechaCreacion = c.DateTime(nullable: false),
+                        FechaModificacion = c.DateTime(nullable: false),
+                        Activo = c.Byte(nullable: false),
+                    })
+                .PrimaryKey(t => t.ContactoID)
+                .ForeignKey("dbo.TipoContactos", t => t.TipoContactoId, cascadeDelete: true)
+                .Index(t => t.TipoContactoId);
+            
+            CreateTable(
+                "dbo.TipoContactos",
+                c => new
+                    {
+                        TipoContactoId = c.Int(nullable: false, identity: true),
+                        Nombre = c.String(),
+                        Descripcion = c.String(),
+                        Activo = c.Byte(nullable: false),
+                    })
+                .PrimaryKey(t => t.TipoContactoId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Contactos", "TipoContactoId", "dbo.TipoContactos");
+            DropIndex("dbo.Contactos", new[] { "TipoContactoId" });
+            DropTable("dbo.TipoContactos");
+            DropTable("dbo.Contactos");
+        }
+    }
+}
